fee4d18e8a9466b2bb4056128e8c38c15795fabb
starbone Jagla <starbone@gmail.com>
2020-05-22 09:21:09 -0600

    styling

--- src/Components/UserDashboard/DashboardLinkMyPatterns.js
+++ src/Components/UserDashboard/DashboardLinkMyPatterns.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import Context from '../../Context';
 import { Link } from 'react-router-dom';
 
-class DashboardLinkFavs extends Component {
+class DashboardLinkMyPatterns extends Component {
     static contextType = Context;
     constructor(props) {
         super(props);
@@ -21,11 +21,11 @@ class DashboardLinkFavs extends Component {
     render() {
         return (
             <section className='dash-fav flex-container'>
-                <Link className="dash-flex-item" to={`/account/${this.props.userId}/favorites`}><div className="dash-img-wrap"><img src={require(`../../Assets/SVG/favorites-dash-panel.svg`)} alt="link to my favorites" className="dashpanel-img"/></div></Link>
+                <Link className="dash-flex-item" to={`/account/${this.props.userId}/contributions`}><div className="dash-img-wrap"><img src={require(`../../Assets/SVG/favorites-dash-panel.svg`)} alt="link to my favorites" className="dashpanel-img"/></div></Link>
             </section>
 
         );
     }
 }
 
-export default DashboardLinkFavs;
+export default DashboardLinkPatterns;
